3153c581c3f2d2967b37a7ed3d81c3bd537a12b2
JonghaLee <jhl9617@gmail.com>
2023-06-05 10:34:12 +0900

    update 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
@@ -66,6 +65,32 @@ function Create(props) {
     </article>
 }
 
+function Update(props) {
+          
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return <article>
+      <h2>Update</h2>
+      <form onSubmit={event=> {
+        event.preventDefault();
+
+        props.onUpdate(title, body);
+
+      }}>
+        <p><input type='text' name='title' placeholder='title' value={title} onChange={event=>{
+          console.log(event.target.value);
+          setTitle(event.target.value);
+        }}/></p>
+        <p><textarea name='body' placeholder='body' value={body} onChange={event=>{
+          console.log(event.target.value);
+          setBody(event.target.value);
+        }}></textarea></p>
+        <p><input type='submit' value='Update'></input></p>
+      </form>
+    </article>
+}
+
+
 function App() {
 
   //아래의 mode, setMode는 마음대로 작명 가능
@@ -81,18 +106,24 @@ function App() {
   ]);
 
   let content = null;
+  let contextControl = null;
+
+  //mode handler
   if(mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, Web"></Article>
   } else if(mode === 'READ') {
     let title, body = null;
     for(let i = 0; i < topics.length; i++) {
-      console.log(topics[i].id, id);
       if(topics[i].id === id) {
         title = topics[i].title;
         body = topics[i].body;
       }
     }
     content = <Article title={title} body={body}></Article>
+    contextControl = <li><a href={'/update/'+id} onClick={event=>{
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>
   } else if(mode === 'CREATE'){
     content = <Create onCreate={(_title, _body)=>{
       const newTopic = {id:nextId, title:_title, body:_body}
@@ -104,8 +135,30 @@ function App() {
       setMode('READ');
       setId(nextId);
       setNextId(nextId+1);
-
     }}></Create>
+  }
+  else if(mode === 'UPDATE'){
+    let title, body = null;
+    for(let i = 0; i < topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      console.log(title, body);
+      const newTopics = [...topics];
+      const updatedTopic = {id:id, title:title, body:body}
+
+      for(let i=0; i < newTopics.length; i++) {
+        if(newTopics[i].id === id) {
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
   }
 
   return (
@@ -122,10 +175,15 @@ function App() {
       {/* Content */}
       {content}
       {/* Create */}
-      <a href="/create" onClick={event=>{
+      <ul>
+      <li>
+        <a href="/create" onClick={event=>{
         event.preventDefault();
         setMode('CREATE');
-      }}>Create</a>
+        }}>Create</a>
+      </li>
+      {contextControl}
+      </ul>
     </div>
   );
 }
